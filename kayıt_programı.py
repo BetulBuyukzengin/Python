5d74b2ce151dcd3b8afc8b2984c5d6d6b94a8d03
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2022-02-09 15:54:14 +0300

    Add files via upload

---
+++ kayıt_programı.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  9 15:12:50 2022
+
+@author: betul
+"""
+
+#*******************************************OYUNCU KAYDETME PROGRAMI*********************************************
+print ("Oyuncu Kaydetme Programına Hoşgeldiniz...")
+ad=input("Adınız: ")
+soyad=input("Soyadınız: ")
+takim=input("Takımınız: ")
+
+bilgiler=[ad,soyad,takim]
+print("Kayıt oldunuz ...")
+
+print("Oyuncunun adı:{}\nOyuncunun soyadi:{}\nOyuncunun takımı:{}\n".format(bilgiler[0],bilgiler[1],bilgiler[2]))
